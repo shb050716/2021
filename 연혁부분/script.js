8416ee79c3bbe5fae60bd408e39a58e7c3848d7e
WEBDESIGN-CLUB\WebDesign <vjvmf1356@gmail.com>
2021-12-04 11:39:27 +0900

    .

---
+++ 연혁부분/script.js
@@ -0,0 +1,20 @@
+$(function () {
+    event();
+    console.log(modalSave())
+})
+
+function event(){
+    $(document)
+        .on('click', '.yearButton', HistoryRegistration)
+        .on('click', '.modal-footer .btn-primary', modalSave)
+}
+
+function HistoryRegistration () {
+    $('.modal').modal('show');
+}
+
+function modalSave () {
+    let saveHistory = $('.modal-body .input_name').val();
+    $('.modal').modal('hide');
+    localStorage
+}
