fce7219603c9f11a01104be8a0bbad08249abfab
WEBDESIGN-CLUB\WebDesign <vjvmf1356@gmail.com>
2021-12-06 20:47:48 +0900

    .

--- 연혁부분/script.js
+++ 연혁부분/script.js
@@ -1,20 +1,69 @@
 $(function () {
     event();
-    console.log(modalSave())
+    let saveLocal = getData('data');
+    let saveIdx = getData('idx');
+    if (saveLocal === null || saveLocal === undefined) {
+        localStorage.data = JSON.stringify([]);
+    }
+    if (saveIdx === null || saveIdx === undefined) {
+        localStorage.idx = JSON.stringify([]);
+    }
 })
 
-function event(){
+const getData = (storage) => {
+    if (!localStorage[storage]) return null;
+    return JSON.parse(localStorage[storage]);
+}
+
+function event() {
     $(document)
         .on('click', '.yearButton', HistoryRegistration)
-        .on('click', '.modal-footer .btn-primary', modalSave)
+        .on('click', '.first', modal1Save)
+        .on('click', '.bodyText .correction', modalCorrection)
+        .on('click','.second',modal2Save)
+}
+
+function HistoryRegistration() {
+    $('.Mou').modal('show');
+}
+
+function modalCorrection() {
+    $('.Mo').modal('show');
+}
+
+function modal1Save() {
+    let localData = JSON.parse(localStorage.data);
+    let localIdx = JSON.parse(localStorage.idx);
+    let saveHistory = $('.modal1_Text').val();
+    let viewText = ``;
+    let id_history = 0;
+    if (saveHistory === '') {
+        return
+    }
+    localData.forEach((e,idx) => {
+        console.log(e);
+        viewText += `<div class="bodyText f" data-id="${idx}">
+            <span>-01.19</span>
+            <span class="localText">${e}</span>
+            <input type="button" class="correction" value="수정">
+                <input type="button" value="삭제">
+            </div>`
+        id_history ++;
+    })
+    localData.push(saveHistory);
+    localIdx.push(id_history);
+    localStorage.data = JSON.stringify(localData);
+    localStorage.idx = JSON.stringify(localIdx);
+    $('.Mou').modal('hide');
+    $('.bodyContents').html(viewText);
 }
 
 
-function HistoryRegistration () {
-    $('.modal').modal('show');
-}
 
-function modalSave () {
-    let saveHistory = $('.modal-body .input_name').val();
-    $('.modal').modal('hide');
+function modal2Save (){
+    let modal_save = $('.modal2_Text').val();
+    if(modal_save === ''){
+        return
+    }
+    $('.Mo').modal('hide');
 }
